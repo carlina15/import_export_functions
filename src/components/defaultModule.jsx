94e7ddfa2c29d8f4838dc3859f874d44c5f29e61
Karla Vujevic <karla_vu@icloud.com>
2023-09-06 18:59:49 +0200

    export-import-funkcije-vjezba

---
+++ src/components/defaultModule.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const MainPart = () => {
+    return (
+        <h2>Main part</h2>
+    )
+}
+
+export default MainPart
---
+++ src/components/otherModules.jsx
@@ -0,0 +1,18 @@
+export const IntroductionSection = () => {
+    return (
+        <h2>Introduction</h2>
+    )
+}
+
+
+export const AboutSection = () => {
+    return (
+        <h2>About</h2>
+    )
+}
+
+export const ConclusionSection = () => {
+    return (
+        <h2>Conclusion</h2>
+    )
+}
